755a2d5989be2872b64d1a923bc79866d13f4ef1
Cliente CONTROL-PC <CltControl@GGIECSOFWRK159.espol.edu.ec>
2018-05-23 14:52:32 -0500

    taller

--- aero_test.py
+++ aero_test.py
@@ -15,9 +15,35 @@ class TestAerolinea(unittest.TestCase):
 		self.assertEqual(valor_total, 1540.00)
 
 	def test_aerolinea_2(self):
-		"""Declarar clases de equivalencia utilizadas"""
-		valor_total=0
-		self.assertEqual(valor_total,0)
+		""" 
+		CE0: Adulto, CE10: Valido, CE21: Europa, CE31: 3, CE40: economica"""
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 3, 'economica')
+		boleto2 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 3, 'economica')
+		boleto3 = pasajero.Pasajero('Tercera', 'Internacional', 'Europa', 3, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,1417.5)
+
+	def test_aerolinea_3(self):
+		""" 
+		CE1: Adulto, Internacional, Europa, 3, economica"""
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 10, 'economica')
+		boleto2 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 10, 'economica')
+		boleto3 = pasajero.Pasajero('Tercera', 'Internacional', 'Europa', 10, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,1512.0)
+
+	def test_aerolinea_4(self):
+		""" 
+		CE0: Adulto, CE10: Valido, CE21: Europa, CE31: 3, CE40: economica"""
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 10, 'ejecutiva')
+		boleto2 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 10, 'ejecutiva')
+		boleto3 = pasajero.Pasajero('Tercera', 'Internacional', 'Europa', 10, 'ejecutiva')
+		lista_pasajero = [boleto1,boleto2,boleto3]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,1572.0)
+
 
 if __name__ == '__main__':
 	unittest.main()
