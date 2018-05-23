ea7e9970316116a0904c3fb308d8302109a86740
Arelys Vintimilla <aremvint@espol.edu.ec>
2018-05-23 15:04:38 -0500

    modificaciones

--- aero_test.py
+++ aero_test.py
@@ -15,9 +15,28 @@ class TestAerolinea(unittest.TestCase):
 		self.assertEqual(valor_total, 1540.00)
 
 	def test_aerolinea_2(self):
-		"""Declarar clases de equivalencia utilizadas"""
-		valor_total=0
-		self.assertEqual(valor_total,0)
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 1, 'economica')
+		boleto2 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 1, 'economica')
+		boleto3 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 1, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero) 
+		self.assertEqual(valor_total, 1470.00)
+
+	def test_aerolinea_3(self):
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 12, 'economica')
+		boleto2 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 12, 'economica')
+		boleto3 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 12, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero) 
+		self.assertEqual(valor_total, 1568.00)
+	
+	def test_aerolinea_4(self):
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 0, 'economica')
+		boleto2 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 0, 'economica')
+		boleto3 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 0, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero) 
+		self.assertEqual(valor_total, 1400.00)
 
 if __name__ == '__main__':
 	unittest.main()
