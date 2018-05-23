9773226ce1b1b02a9b2fd5ad080c559a3d869b30
Maria Belen Guaranda <mguarand@espol.edu.ec>
2018-05-23 15:13:24 -0500

    tests passed

--- aero_test.py
+++ aero_test.py
@@ -14,10 +14,26 @@ class TestAerolinea(unittest.TestCase):
 		valor_total = calculo_funciones.obtener_total(lista_pasajero)
 		self.assertEqual(valor_total, 1540.00)
 
-	def test_aerolinea_2(self):
-		"""Declarar clases de equivalencia utilizadas"""
-		valor_total=0
-		self.assertEqual(valor_total,0)
+	def test_aerolinea_enero(self):
+		"""Vuelo internacional, Europa, 1 adulto, enero, económica"""
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 1, 'economica')
+		lista_pasajero = [boleto1]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,525)
+
+	def test_aerolinea_abril(self):
+		"""Vuelo internacional, Europa, 1 adulto, abril, económica"""
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 4, 'economica')
+		lista_pasajero = [boleto1]
+		valor_total =  calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,550)
+
+	def test_aerolinea_septiembre(self):
+		"""Vuelo internacional, Europa, 1 adulto, septiembre, económica"""
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 9, 'economica')
+		lista_pasajero = [boleto1]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,560)
 
 if __name__ == '__main__':
 	unittest.main()
