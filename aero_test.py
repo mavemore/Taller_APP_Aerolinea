49faeb770a042924b048554c4b4f3f2929209920
jorge18link <jorge_18link@hotmail.com>
2018-05-23 15:08:56 -0500

    modificaciones
    termine el taller

--- aero_test.py
+++ aero_test.py
@@ -16,8 +16,36 @@ class TestAerolinea(unittest.TestCase):
 
 	def test_aerolinea_2(self):
 		"""Declarar clases de equivalencia utilizadas"""
-		valor_total=0
-		self.assertEqual(valor_total,0)
+		"P1"
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 1, 'economica')
+		boleto2 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 1, 'economica')
+		boleto3 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 1, 'economica')
+		boleto4 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 1, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3,boleto4]
+		valor_total=calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,2100)
+
+	def test_aerolinea_3(self):
+		"""Declarar clases de equivalencia utilizadas"""
+		"P3"
+		boleto1 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 4, 'economica')
+		boleto2 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 4, 'economica')
+		boleto3 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 4, 'economica')
+		boleto4 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 4, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3,boleto4]
+		valor_total=calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,2200)
+		
+	def test_aerolinea_4(self):
+		"""Declarar clases de equivalencia utilizadas"""
+		"P4"
+		boleto1 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 11, 'economica')
+		boleto2 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 11, 'economica')
+		boleto3 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 11, 'economica')
+		boleto4 = pasajero.Pasajero('Niño', 'Internacional', 'Europa', 11, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3,boleto4]
+		valor_total=calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,1792)
 
 if __name__ == '__main__':
 	unittest.main()
