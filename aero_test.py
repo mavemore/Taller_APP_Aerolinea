596a80b85c00e8529bc20be6ebf0d4e291f63f11
Renato Illescas <rillesca@espol.edu.ec>
2018-05-23 15:10:31 -0500

    Add files via upload
    taller realizado

--- aero_test.py
+++ aero_test.py
@@ -16,8 +16,15 @@ class TestAerolinea(unittest.TestCase):
 
 	def test_aerolinea_2(self):
 		"""Declarar clases de equivalencia utilizadas"""
-		valor_total=0
-		self.assertEqual(valor_total,0)
+		"""P1"""
+		boleto1 = pasajero.Pasajero('Adulto','Internacional','Europa',3,'economica')
+		"""P2"""
+		boleto2 = pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 9, 'economica')
+		"""P3"""
+		boleto3= pasajero.Pasajero('Adulto', 'Internacional', 'Europa', 4, 'economica')
+		lista_pasajero = [boleto1,boleto2,boleto3]
+		valor_total = calculo_funciones.obtener_total(lista_pasajero)
+		self.assertEqual(valor_total,1635)
 
 if __name__ == '__main__':
 	unittest.main()
